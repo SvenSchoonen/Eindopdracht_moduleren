ac9dda213f0841ce2d379aa0885c49c6f5916a45
SvenSchoonen <s.schoonen@st.hanze.nl>
2025-01-14 15:21:03 +0100

    mgrid

--- model/start_model.py
+++ model/start_model.py
@@ -18,9 +18,11 @@ class Cell:
     def __init__(self, parameters):
         self.cell_type = cell_type
         self.position = position  # Position in grid
+        print("postiosn", position)
         self.alive = True
         self.age = 0 # iteration
         self.mutation_count = 0  # Tracks the number of mutations the cell has undergone 
+        self.state = 0  # 0 = v1 1 = v2, 2= v3, 3 = v4
 
     def apoptose():
         return False
@@ -32,7 +34,7 @@ class Cell:
         pass
         
     def migratie():
-        zelf.position = newpostion # verplaatsen van een cell als er plek is
+        zelf.position = newpostion # Kans op verplaatsen van een cell als er plek is
         
     def mutation_count():
         pass # mutatie wnr de cell muteerd en hoeveel mutaties
@@ -45,22 +47,44 @@ class Cell:
         
         
         
-def initialize_grid(grid_size, cell_types, normal_ratio=0.5, tumor_ratio=0.05, stem_ratio=0.05):
+def initialize_grid(grid_size, cell_types, normal_ratio=0.9, tumor_ratio=0.1, stem_ratio=0.05): # aanmaak levensduur cel 
     grid = np.empty(grid_size, dtype=object)
+
     for x in range(grid_size[0]):
         for y in range(grid_size[1]):
             for z in range(grid_size[2]):
                 rand_value = random.random() # make random number between 0 and one to 
                 if rand_value < normal_ratio:
                     cell_type = cell_types["normal"]
-                    print("normal cell made on", x, y, z)
+                    #print("normal cell made on", x, y, z)
                     grid[x,y,z] = cell_types['normal']
                 if rand_value < tumor_ratio:
-					print("tumor cell made on", x, y, z)
-                    cell_type = cell_types["stem"]
-                    grid[x,y,z] = cell_types['normal']
-    print(grid)
+                    #print("tumor cell made on", x, y, z)
+                    grid[x,y,z] = cell_types['tumor']
+                if rand_value < stem_ratio:
+                    #print("Tumor stem cell made on", x, y, z)
+                    grid[x,y,z] = cell_types['stem']
+                # make blood vessel with heat map
+                
+                
+                
+    #print(grid)
     return 
-print(config["cell_types"])
+# print(config["cell_types"])
+  # Angiogenesis
+def make_bloodvessel_grid(blood_vessel_place, grid_size, vessel_thickness): # Maybe add hor and vert
+    print("start")
+    grid_size = [6,6,6]
+    blood_vessel_place = [2,2]   # going up in Z 
+    x = blood_vessel_place[0]
+    y = blood_vessel_place[1]
+    blood_vessel_height = 9
+    blood_vessel_thickness = [1, 1] # x , y , z   
+        
+    #print(grid)     
+    return np.mgrid[:grid_size[0], :grid_size[1], :grid_size[2]].T.reshape((-1,vessel_thickness))	
+print(make_bloodvessel_grid(config["vessel_place"], config["vessel_size"],  config["vessel_thickness"]))
 initialize_grid(config["grid_size"], config["cell_types"])
 # Models ...
+
+
